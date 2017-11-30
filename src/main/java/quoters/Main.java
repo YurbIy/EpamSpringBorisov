9b1743fc917a3f7cabe78f9d37e03aba54bf4d1a
Iurii Novikov <yunov1@gmail.com>
2017-11-30 19:57:23 +0300

    I'll be back quot worked

---
+++ src/main/java/quoters/Main.java
@@ -0,0 +1,11 @@
+package quoters;
+
+import com.sun.org.apache.bcel.internal.util.ClassPath;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+        context.getBean(TerminatorQouter.class).sayQuote();
+    }
+}
