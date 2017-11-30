08b2c3ea675c1313e2018a515a5cdb9750d809e7
Iurii Novikov <yunov1@gmail.com>
2017-12-01 00:32:25 +0300

    Removed excess invokation in Main

--- src/main/java/quoters/Main.java
+++ src/main/java/quoters/Main.java
@@ -6,6 +6,5 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-        context.getBean(quoters.Quoter.class).sayQuote();
     }
 }
--- src/main/java/quoters/PostProxyInvokerContextListener.java
+++ src/main/java/quoters/PostProxyInvokerContextListener.java
@@ -9,6 +9,8 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.event.ContextRefreshedEvent;
 
+import java.lang.reflect.Method;
+
 
 public class PostProxyInvokerContextListener implements ApplicationListener<ContextRefreshedEvent>
 {
@@ -23,6 +25,20 @@ public class PostProxyInvokerContextListener implements ApplicationListener<Cont
         for (String name : names) {
             BeanDefinition beanDefinition = factory.getBeanDefinition(name);
             String originalClassName = beanDefinition.getBeanClassName();
+            try {
+                Class<?> originalClass = Class.forName(originalClassName);
+                Method[] methods = originalClass.getMethods();
+                for (Method method : methods) {
+                    if(method.isAnnotationPresent(PostProxy.class)){
+                        Object bean = context.getBean(name);
+                        Method currMethod = bean.getClass().getMethod(method.getName(), method.getParameterTypes());
+                        currMethod.invoke(bean);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
         }
     }
 }
