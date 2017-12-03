61054ef454d22df73135237556f8a54b27740def
Iurii Novikov <yunov1@gmail.com>
2017-12-03 20:06:08 +0300

    intermediat commit

---
+++ src/main/java/quoters/PostProxyInvokerContextListener.java
@@ -0,0 +1,24 @@
+package quoters;
+
+
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+
+public class PostProxyInvokerContextListener implements ApplicationListener<ContextRefreshedEvent> {
+
+    private ConfigurableListableBeanFactory factory;
+
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        ApplicationContext context = event.getApplicationContext();
+        String[] names = context.getBeanDefinitionNames();
+        for (String name : names){
+            factory.
+        }
+    }
+
+}
