6ba7bb644beefd7654a008a9123a185a2b1c19a1
Iurii Novikov <yunov1@gmail.com>
2017-12-03 21:54:26 +0300

    working

---
+++ src/main/java/screensaver/Config.java
@@ -0,0 +1,26 @@
+package screensaver;
+
+import org.springframework.context.annotation.*;
+import org.springframework.stereotype.Component;
+
+import java.awt.*;
+import java.util.Random;
+
+@Configuration
+@ComponentScan(basePackages = "screensaver")
+public class Config {
+    @Bean
+    @Scope("prototype")
+    public Color color(){
+        Random random = new Random();
+        return new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255));
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+        while (true) {
+            context.getBean(ColorFrame.class).showOnRanromPlace();
+            Thread.sleep(1000);
+        }
+    }
+}
