8dacec35d12feb3de6199615fc8d9ab2c4050339
Iurii Novikov <yunov1@gmail.com>
2017-12-03 19:56:44 +0300

    PostPr annotation is created

--- src/main/java/quoters/Main.java
+++ src/main/java/quoters/Main.java
@@ -6,15 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-        while (true) {
-            try {
-                Thread.sleep(100);
-            }
-            catch (InterruptedException e){
-                e.printStackTrace();
-            }
         context.getBean(quoters.Quoter.class).sayQuote();
-        }
-
     }
+
 }
