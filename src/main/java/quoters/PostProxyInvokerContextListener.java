c89f107a76754dc70cbfdfb24a4ba5aabae19936
Iurii Novikov <yunov1@gmail.com>
2017-12-03 20:24:39 +0300

    nullPointer thown

--- src/main/java/quoters/PostProxyInvokerContextListener.java
+++ src/main/java/quoters/PostProxyInvokerContextListener.java
@@ -1,11 +1,14 @@
 package quoters;
 
 
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+
+import java.lang.reflect.Method;
 
 public class PostProxyInvokerContextListener implements ApplicationListener<ContextRefreshedEvent> {
 
@@ -17,7 +20,21 @@ public class PostProxyInvokerContextListener implements ApplicationListener<Cont
         ApplicationContext context = event.getApplicationContext();
         String[] names = context.getBeanDefinitionNames();
         for (String name : names){
-            factory.
+            BeanDefinition beanDefinition = factory.getBeanDefinition(name);
+            String originalClassName = beanDefinition.getBeanClassName();
+            try {
+                Class<?> originalClass = Class.forName(originalClassName);
+                Method[] methods = originalClass.getMethods();
+                for(Method method : methods){
+                    if (method.isAnnotationPresent(PostProxy.class)){
+                        Object bean = context.getBean(name);
+                        Method currentMethod = bean.getClass().getMethod(method.getName(), method.getParameterTypes());
+                        currentMethod.invoke(bean);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 
